9e92a2c9deddaef1fd212d1f08dc97ab00c8ca56
bspriester <54115589+bspriester@users.noreply.github.com>
2021-08-19 23:23:31 -0500

    Add files via upload

---
+++ submit_reminder.php
@@ -0,0 +1,283 @@
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>Now Or Later Reminders</title>
+    <style type="text/css">
+                html { color: #ddd; font-family: sans-serif; font-size: 16px; background-color: #000000; text-align: center; }
+                header { text-align: center;}
+                body {}
+		footer {
+		position: absolute;
+		bottom:0;
+		width: 99.5%;
+		height:60px;
+		}
+		img {
+		margin-left: auto;
+		margin-right: auto;
+		display: none;
+		width: 70%
+		}
+                .main_body {margin: 30px;}
+                ul {
+                padding-left: 28px;
+                list-style-position: outside;
+                }
+                .navbar {
+                list-style-type: none;
+                margin: 0;
+                padding: 0;
+                overflow: hidden;
+                background-color: #333;
+		font-size: 18px;
+                }
+                .navbar_buttons {
+                display: block;
+                color: #ddd;
+                text-align: center;
+                padding: 14px 16px;
+                text-decoration: none;
+                float: right;
+                }
+                .navbar_links {
+                display: block;
+                color: #ddd;
+                text-align: center;
+                padding: 14px 16px;
+                text-decoration: none;
+                float: left;
+                }
+                .navbar_links:hover {
+                background-color: #111;
+		}
+                .light-mode {
+                background-color: white;
+                color: black;
+		background-image: url(https://i.imgur.com/3uiFRXH.jpg);
+                }
+		.light-mode-navbar {
+                background-color: gray;
+                color: black;
+                }
+        </style>
+</head>
+<body>
+    <!-- API stuff -->
+    <?php
+    //Get visitor IP, taking into account proxy possibility.
+        function getIp() {
+            $ip = $_SERVER['REMOTE_ADDR'];
+            if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+                $ip = $_SERVER['HTTP_CLIENT_IP'];
+            } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+                $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+            }
+            return $ip;
+        }
+        //Commenting out the below for testing.
+        $getIp = getIp();
+        //$getIp = "76.231.70.33";
+        //$getIp = "8.8.8.8";
+        $apiWithIp = "http://ip-api.com/json/" . $getIp;
+        $getInfo = file_get_contents($apiWithIp);
+        $decode = json_decode($getInfo);
+        echo $decode->timezone;
+        //$ip = getIPAddress();
+    ?>
+    <!-- Reminder set message -->
+    <?php
+        $reminderSetData = $_POST["reminder"];
+        $reminderSetDate = $_POST["time"];
+        $reminderSetMessage = "Reminder of $reminderSetData set for time $reminderSetDate";
+        if ($reminderSetData == ""){
+            $reminderSetMessage = "Submit new reminder below.";
+        }
+        echo $reminderSetMessage; 
+    ?>
+    <br>
+    <!-- Test for row forms -->
+    <?php
+        $editDate = $_POST["editDate"];
+        $editData = $_POST["editData"];
+        $testEdit = "Test for $editData and $editDate";
+        echo $testEdit;
+    ?>
+    <!-- Set reminder form --> 
+    <div class="main_page">
+        <form action="submit_reminder.php" method="post">
+            Reminder: <input type="text" name="reminder"><br>
+            Time: <input type="text" name="time" placeholder="YYYY-MM-DD hh:mm:ss"><br>
+            <input type="submit">
+        </form>
+    </div>
+    <!-- MySQL setup -->
+    <?php
+        //Mysql connections:
+        $con = mysqli_connect('localhost:3306', 'root', 'Bsz94290');
+        //Check for connection error:
+        if (!$con) {
+            echo "Error: Unable to connect to MySQL." . PHP_EOL;
+            echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+            echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+            exit;
+        }
+        //Create reminders_database database if it doesn't exist yet:
+        $sql = "CREATE DATABASE IF NOT EXISTS reminders_database";
+        $con->query($sql);
+        //Select the reminders_database database:
+        $sql = "use reminders_database";
+        $con->query($sql);
+        //Create the reminders_table if it doesn't exist:
+        $sql = "CREATE TABLE IF NOT EXISTS reminders_table (ReminderId int, ReminderData text, ReminderDate datetime);";
+        $con->query($sql);
+        //Put the reminder data and time info from the reminder_app.php page into variables:
+        $id = random_int(0, 1000000000);
+        $reminder = $_POST["reminder"];
+        $time = $_POST["time"];
+        //Create new row in reminders_table:
+        $sql = "INSERT INTO reminders_table(ReminderId, ReminderData, ReminderDate) VALUES (\"$id\", \"$reminder\", \"$time\");";
+        $con->query($sql);
+        //Update row in reminders_table:
+        $updateId = $_POST["editId"];
+        $updateData = $_POST["editData"];
+        $updateDate = $_POST["editDate"];
+        $sql = "UPDATE reminders_table SET ReminderData = \"$updateData\", ReminderDate = \"$updateDate\" WHERE ReminderId = $updateId;";
+        $con->query($sql);
+        //Delete row in reminders_table:
+        $updateIdDelete = $_POST["deleteEditId"];
+        $sql = "DELETE FROM reminders_table WHERE ReminderId = $updateIdDelete;";
+        $con->query($sql);
+    ?>
+    <!-- Get reminders: -->
+    <?php 
+        $getTime = date("Y-m-d G:i:s"); 
+        $dueFound = 0;
+    ?>
+    <table width="100%"  border="1">
+        <tr>
+            <th>Time</th>
+            <th>Reminder</th>
+            <th>Submit time or reminder changes</th>
+            <th>Delete reminder</th>
+        </tr>
+        <?php
+        $sql = "SELECT * FROM reminders_table WHERE ReminderDate > CURRENT_DATE ORDER BY ReminderDate DESC;";
+        $query=mysqli_query($con, $sql);
+        $cnt=1;
+        //Commenting out the below since I believe it was for testing:
+        //$fetchedInfo = mysqli_fetch_array($query);
+        //echo $fetchedInfo['ReminderDate'];
+        $previousDate = $null;
+        while($row=mysqli_fetch_array($query))
+        {
+            $currentDate = $row['ReminderDate'];
+            if(strncmp($currentDate, $previousDate, 10) != 0) {
+        ?>
+                <tr>
+                    <td><b><?php echo "DAY " . substr($currentDate, 0, 10);?></b></td>
+                    <td><?php echo "-";?></td>
+                    <td><?php echo "-";?></td>
+                    <td><?php echo "-";?></td>
+                </tr>
+                <form action="submit_reminder.php" method="post">
+                    <tr>
+                        <td><input type="text" name="editDate" value="<?php echo $row['ReminderDate']; ?>"></td>
+                        <td><input type="text" name="editData" value="<?php echo $row['ReminderData']; ?>"></td>
+                        <td>Submit changes <input type="submit">
+                        <input type="hidden" name="editId" value="<?php echo $row['ReminderId']; ?>"></td>
+                </form>
+                <form action="submit_reminder.php" method="post">
+                        <td>Delete <input type="submit">
+                        <input type="hidden" name="deleteEditId" value="<?php echo $row['ReminderId']; ?>">
+                        </td> 
+                    </tr>
+                </form>
+                <?php
+            }
+            else { 
+            ?>
+                <form action="submit_reminder.php" method="post">
+                    <tr>
+                        <td><input type="text" name="editDate" value="<?php echo $row['ReminderDate']; ?>"></td>
+                        <td><input type="text" name="editData" value="<?php echo $row['ReminderData']; ?>"></td>
+                        <td>Submit changes <input type="submit">
+                        <input type="hidden" name="editId" value="<?php echo $row['ReminderId']; ?>"></td>
+                </form>
+                <form action="submit_reminder.php" method="post">
+                        <td>Delete <input type="submit">
+                        <input type="hidden" name="deleteEditId" value="<?php echo $row['ReminderId']; ?>">
+                        </td> 
+                    </tr>
+                </form>
+        <?php
+            }
+            $cnt=$cnt+1;
+            $previousDate = $row['ReminderDate'];
+        } 
+        ?>
+    </table>
+    <br><p><b>^ REMINDERS DUE ^</b></p><br>
+    <table width="100%"  border="1">
+        <tr>
+            <th>Time</th>
+            <th>Reminder</th>
+            <th>Submit time or reminder changes</th>
+        </tr>
+        <?php
+        $sql = "SELECT * FROM reminders_table WHERE ReminderDate < CURRENT_DATE ORDER BY ReminderDate DESC;";
+        $query=mysqli_query($con, $sql);
+        $cnt=1;
+        //Commenting out the below since I believe it was for testing:
+        //$fetchedInfo = mysqli_fetch_array($query);
+        //echo $fetchedInfo['ReminderDate'];
+        $previousDate = $null;
+        while($row=mysqli_fetch_array($query))
+        {
+            $currentDate = $row['ReminderDate'];
+            if(strncmp($currentDate, $previousDate, 10) != 0) {
+        ?>
+                <tr>
+                    <td><b><?php echo "DAY " . substr($currentDate, 0, 10);?></b></td>
+                    <td><?php echo "-";?></td>
+                    <td><?php echo "-";?></td>
+                    <td><?php echo "-";?></td>
+                </tr>
+                <form action="submit_reminder.php" method="post">
+                    <tr>
+                        <td><input type="text" name="editDate" value="<?php echo $row['ReminderDate']; ?>"></td>
+                        <td><input type="text" name="editData" value="<?php echo $row['ReminderData']; ?>"></td>
+                        <td>Submit changes <input type="submit">
+                        <input type="hidden" name="editId" value="<?php echo $row['ReminderId']; ?>"></td>
+                </form>
+                <form action="submit_reminder.php" method="post">
+                        <td>Delete <input type="submit">
+                        <input type="hidden" name="deleteEditId" value="<?php echo $row['ReminderId']; ?>">
+                        </td> 
+                    </tr>
+                </form>
+                <?php
+            }
+            else { 
+            ?>
+                <form action="submit_reminder.php" method="post">
+                    <tr>
+                        <td><input type="text" name="editDate" value="<?php echo $row['ReminderDate']; ?>"></td>
+                        <td><input type="text" name="editData" value="<?php echo $row['ReminderData']; ?>"></td>
+                        <td>Submit changes <input type="submit">
+                        <input type="hidden" name="editId" value="<?php echo $row['ReminderId']; ?>"></td>
+                </form>
+                <form action="submit_reminder.php" method="post">
+                        <td>Delete <input type="submit">
+                        <input type="hidden" name="deleteEditId" value="<?php echo $row['ReminderId']; ?>">
+                        </td> 
+                    </tr>
+                </form>
+        <?php
+            }
+            $cnt=$cnt+1;
+            $previousDate = $row['ReminderDate'];
+        } 
+        ?>
+    </table>
+</body>
+</html>
